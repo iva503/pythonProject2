c750bb442e98319995020c6fdb4844897ef998f2
antonio-marius.trico <antonio-marius.tricolici@prolion.com>
2024-01-24 20:19:18 +0100

    I changed a space

--- game_objects/bullet.py
+++ game_objects/bullet.py
@@ -10,12 +10,11 @@ class bullet:
         self.screen = screen
         self.position = initial_positon
         self.image = pygame.image.load("img_4.png")
-        self.image = pygame.transform.scale(self.image,(self.image.get_width()*0.05,self.image.get_height()*0.05))
+        self.image = pygame.transform.scale(self.image, (self.image.get_width() * 0.05, self.image.get_height() * 0.05))
         self.image = pygame.transform.rotate(self.image, 90)
+
     def draw(self):
         self.screen.blit(self.image, self.position, self.image.get_rect())
 
     def increase_position(self, delta_time):
         self.position.y -= 500 * delta_time
-
-
